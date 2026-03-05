fbe523c10a070cdae79b7ed2183f7cae23fb0211
SubParSuperCar <elliot_grimm_2008@yahoo.com>
2026-03-05 15:23:13 -0800

    Reformat code

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -5,21 +5,24 @@ import com.ctre.phoenix6.controls.DutyCycleOut;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystem {
-    
     // --- Motor declarations ---
     // Add more TalonFX objects here if you have multiple intake motors
-    private final TalonFX intakeMotor1 = new TalonFX(20); // ← Set your CAN ID here
+    private final TalonFX intakeMotor1 = new TalonFX(20);
     private final TalonFX intakeMotor2 = new TalonFX(20);
+
     // Control request (open-loop percent output, -1.0 to 1.0)
     private final DutyCycleOut motorRequest = new DutyCycleOut(0);
+
     // --- Intake In ---
     public void intakeIn() {
         intakeMotor1.setControl(motorRequest.withOutput(0.75)); // 75% forward
     }
+    
     // --- Intake Out (eject) ---
     public void intakeOut() {
         intakeMotor1.setControl(motorRequest.withOutput(-0.75)); // 75% reverse
     }
+
     // --- Stop ---
     public void stop() {
         intakeMotor1.setControl(motorRequest.withOutput(0));
