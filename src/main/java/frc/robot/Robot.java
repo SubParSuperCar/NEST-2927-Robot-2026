bd84595ec805350e5ba926e1189e45f64ef3111c
Master12329 <masterdylanh9@gmail.com>
2026-03-07 12:15:30 -0800

    Disable deploy for safety

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,82 +10,85 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
-	private Command m_autonomousCommand;
+  private Command m_autonomousCommand;
 
-	private final RobotContainer m_robotContainer;
+  @SuppressWarnings("unused")
+  private final RobotContainer m_robotContainer;
 
-	/* log and replay timestamp and joystick data */
-	private final HootAutoReplay m_timeAndJoystickReplay = new HootAutoReplay().withTimestampReplay()
-			.withJoystickReplay();
+  /* log and replay timestamp and joystick data */
+  private final HootAutoReplay m_timeAndJoystickReplay = new HootAutoReplay().withTimestampReplay()
+      .withJoystickReplay();
 
-	public Robot() {
-		m_robotContainer = new RobotContainer();
-	}
+  public Robot() {
+    m_robotContainer = new RobotContainer();
+  }
 
-	@Override
-	public void robotPeriodic() {
-		m_timeAndJoystickReplay.update();
-		CommandScheduler.getInstance().run();
-	}
+  @Override
+  public void robotPeriodic() {
+    m_timeAndJoystickReplay.update();
+    CommandScheduler.getInstance().run();
+  }
 
-	@Override
-	public void disabledInit() {
-	}
+  @Override
+  public void disabledInit() {
+  }
 
-	@Override
-	public void disabledPeriodic() {
-	}
+  @Override
+  public void disabledPeriodic() {
+  }
 
-	@Override
-	public void disabledExit() {
-	}
+  @Override
+  public void disabledExit() {
+  }
 
-	@Override
-	public void autonomousInit() {
-		m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+  @Override
+  public void autonomousInit() {
+    /*
+     * m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+     *
+     * if (m_autonomousCommand != null) {
+     * CommandScheduler.getInstance().schedule(m_autonomousCommand);
+     * }
+     */
+  }
 
-		if (m_autonomousCommand != null) {
-			CommandScheduler.getInstance().schedule(m_autonomousCommand);
-		}
-	}
+  @Override
+  public void autonomousPeriodic() {
+  }
 
-	@Override
-	public void autonomousPeriodic() {
-	}
+  @Override
+  public void autonomousExit() {
+  }
 
-	@Override
-	public void autonomousExit() {
-	}
+  @Override
+  public void teleopInit() {
+    if (m_autonomousCommand != null) {
+      CommandScheduler.getInstance().cancel(m_autonomousCommand);
+    }
+  }
 
-	@Override
-	public void teleopInit() {
-		if (m_autonomousCommand != null) {
-			CommandScheduler.getInstance().cancel(m_autonomousCommand);
-		}
-	}
+  @Override
+  public void teleopPeriodic() {
+  }
 
-	@Override
-	public void teleopPeriodic() {
-	}
+  @Override
+  public void teleopExit() {
+  }
 
-	@Override
-	public void teleopExit() {
-	}
+  @Override
+  public void testInit() {
+    CommandScheduler.getInstance().cancelAll();
+  }
 
-	@Override
-	public void testInit() {
-		CommandScheduler.getInstance().cancelAll();
-	}
+  @Override
+  public void testPeriodic() {
+  }
 
-	@Override
-	public void testPeriodic() {
-	}
+  @Override
+  public void testExit() {
+  }
 
-	@Override
-	public void testExit() {
-	}
-
-	@Override
-	public void simulationPeriodic() {
-	}
+  @Override
+  public void simulationPeriodic() {
+  }
 }
