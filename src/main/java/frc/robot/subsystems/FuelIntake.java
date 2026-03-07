cd0191dddf041e0122e0f8561284e23efe183aa6
Master12329 <masterdylanh9@gmail.com>
2026-03-06 19:54:41 -0800

    Add Spotlight linter & run format pass

--- src/main/java/frc/robot/subsystems/FuelIntake.java
+++ src/main/java/frc/robot/subsystems/FuelIntake.java
@@ -1,35 +1,34 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.controls.DutyCycleOut;
-
+import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class FuelIntake extends SubsystemBase {
-    private final TalonFX intakeMotor = new TalonFX(99);
-    private final TalonFX deployMotor = new TalonFX(99);
+  private final TalonFX intakeMotor = new TalonFX(99);
+  private final TalonFX deployMotor = new TalonFX(99);
 
-    private final DutyCycleOut motorRequest = new DutyCycleOut(0);
+  private final DutyCycleOut motorRequest = new DutyCycleOut(0);
 
-    private static final double MOTOR_OUTPUT = 1;
+  private static final double MOTOR_OUTPUT = 1;
 
-    public void intakeIn() {
-        intakeMotor.setControl(motorRequest.withOutput(MOTOR_OUTPUT));
-    }
+  public void intakeIn() {
+    intakeMotor.setControl(motorRequest.withOutput(MOTOR_OUTPUT));
+  }
 
-    public void intakeOut() {
-        intakeMotor.setControl(motorRequest.withOutput(-MOTOR_OUTPUT));
-    }
+  public void intakeOut() {
+    intakeMotor.setControl(motorRequest.withOutput(-MOTOR_OUTPUT));
+  }
 
-    public void stop() {
-        intakeMotor.setControl(motorRequest.withOutput(0));
-    }
+  public void stop() {
+    intakeMotor.setControl(motorRequest.withOutput(0));
+  }
 
-    public void deployOn() {
-        deployMotor.setControl(motorRequest.withOutput(MOTOR_OUTPUT));
-    }
+  public void deployOn() {
+    deployMotor.setControl(motorRequest.withOutput(MOTOR_OUTPUT));
+  }
 
-    public void deployOff() {
-        deployMotor.setControl(motorRequest.withOutput(0));
-    }
+  public void deployOff() {
+    deployMotor.setControl(motorRequest.withOutput(0));
+  }
 }
--- src/main/java/frc/robot/subsystems/FuelShooter.java
+++ src/main/java/frc/robot/subsystems/FuelShooter.java
@@ -1,41 +1,39 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.controls.DutyCycleOut;
-
+import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class FuelShooter extends SubsystemBase {
-    private final TalonFX leftMotor1 = new TalonFX(99);
-    private final TalonFX leftMotor2 = new TalonFX(99);
-    private final TalonFX rightMotor1 = new TalonFX(99);
-    private final TalonFX rightMotor2 = new TalonFX(99);
+  private final TalonFX leftMotor1 = new TalonFX(99);
+  private final TalonFX leftMotor2 = new TalonFX(99);
+  private final TalonFX rightMotor1 = new TalonFX(99);
+  private final TalonFX rightMotor2 = new TalonFX(99);
 
-    private final DutyCycleOut dutyCycleRequest = new DutyCycleOut(0);
+  private final DutyCycleOut dutyCycleRequest = new DutyCycleOut(0);
 
-    private static final double LEFT_MOTOR_PAIR_OUTOUT = 1;
-    private static final double RIGHT_MOTOR_PAIR_OUTPUT = 1;
+  private static final double LEFT_MOTOR_PAIR_OUTOUT = 1;
+  private static final double RIGHT_MOTOR_PAIR_OUTPUT = 1;
 
-    public FuelShooter() {
-    }
+  public FuelShooter() {}
 
-    public void runLeftPair() {
-        leftMotor1.setControl(dutyCycleRequest.withOutput(LEFT_MOTOR_PAIR_OUTOUT));
-        leftMotor2.setControl(dutyCycleRequest.withOutput(LEFT_MOTOR_PAIR_OUTOUT));
-    }
+  public void runLeftPair() {
+    leftMotor1.setControl(dutyCycleRequest.withOutput(LEFT_MOTOR_PAIR_OUTOUT));
+    leftMotor2.setControl(dutyCycleRequest.withOutput(LEFT_MOTOR_PAIR_OUTOUT));
+  }
 
-    public void stopLeftPair() {
-        leftMotor1.setControl(dutyCycleRequest.withOutput(0));
-        leftMotor2.setControl(dutyCycleRequest.withOutput(0));
-    }
+  public void stopLeftPair() {
+    leftMotor1.setControl(dutyCycleRequest.withOutput(0));
+    leftMotor2.setControl(dutyCycleRequest.withOutput(0));
+  }
 
-    public void runRightPair() {
-        rightMotor1.setControl(dutyCycleRequest.withOutput(RIGHT_MOTOR_PAIR_OUTPUT));
-        rightMotor2.setControl(dutyCycleRequest.withOutput(RIGHT_MOTOR_PAIR_OUTPUT));
-    }
+  public void runRightPair() {
+    rightMotor1.setControl(dutyCycleRequest.withOutput(RIGHT_MOTOR_PAIR_OUTPUT));
+    rightMotor2.setControl(dutyCycleRequest.withOutput(RIGHT_MOTOR_PAIR_OUTPUT));
+  }
 
-    public void stopRightPair() {
-        rightMotor1.setControl(dutyCycleRequest.withOutput(0));
-        rightMotor2.setControl(dutyCycleRequest.withOutput(0));
-    }
+  public void stopRightPair() {
+    rightMotor1.setControl(dutyCycleRequest.withOutput(0));
+    rightMotor2.setControl(dutyCycleRequest.withOutput(0));
+  }
 }
