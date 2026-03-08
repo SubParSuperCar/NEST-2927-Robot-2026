c77085abfaafa3ed9f59533d17749c057d6956b5
Noodle <elliot_grimm_2008@yahoo.com>
2026-03-08 15:13:35 -0700

    Reformat and remove redundant explicit multiplier var

--- src/main/java/frc/robot/subsystems/FuelShooter.java
+++ src/main/java/frc/robot/subsystems/FuelShooter.java
@@ -10,31 +10,33 @@ public class FuelShooter extends SubsystemBase {
   private final TalonFX kickerMotor2 = new TalonFX(28, "Default Name");
   private final TalonFX shooterMotor1 = new TalonFX(3, "Default Name");
   private final TalonFX shooterMotor2 = new TalonFX(26, "Default Name");
-  // Vars
-  private double ShooterMotorPower = 1;
-  private double KickerMotorsPower = 0.8;
-  private double ShooterMotorsPower = 0.6;
+  
+  private double KickerMotorOutput = 0.8;
+  private double ShooterMotorOutput = 0.6;
 
-  private final TalonFX[] kickermotors = { kickerMotor1, kickerMotor2, };
-  private final TalonFX[] shootermotors = { shooterMotor1, shooterMotor2 };
+  private final TalonFX[] kickerMotors = { kickerMotor1, kickerMotor2, };
+  private final TalonFX[] shooterMotors = { shooterMotor1, shooterMotor2 };
+  
   private final DutyCycleOut motorRequest = new DutyCycleOut(0);
 
   public FuelShooter() {
   }
 
   private void setOutputAll(double output) {
-    var request1 = motorRequest.withOutput(output * KickerMotorsPower);
-    var request2 = motorRequest.withOutput(output * ShooterMotorsPower);
-    for (TalonFX motor : kickermotors) {
+    var request1 = motorRequest.withOutput(output * KickerMotorPower);
+    var request2 = motorRequest.withOutput(output * ShooterMotorPower);
+    
+    for (TalonFX motor : kickerMotors) {
       motor.setControl(request1);
     }
-    for (TalonFX motor : shootermotors) {
+    
+    for (TalonFX motor : shooterMotors) {
       motor.setControl(request2);
     }
   }
 
   public void run() {
-    setOutputAll(ShooterMotorPower);
+    setOutputAll(1);
   }
 
   public void stop() {
