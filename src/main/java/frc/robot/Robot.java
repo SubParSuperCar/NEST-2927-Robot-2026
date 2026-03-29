e8514b3bb06a2b908624edc536cee94710ebc07f
SubParSuperCar <elliot_grimm_2008@yahoo.com>
2026-03-29 08:57:31 -0700

    Add infrastucture for control negation button

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -45,19 +45,15 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
-    /*
-     * m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-     *
-     * if (m_autonomousCommand != null) {
-     * CommandScheduler.getInstance().schedule(m_autonomousCommand);
-     * }
-     */
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+      if (m_autonomousCommand != null) {
+        CommandScheduler.getInstance().schedule(m_autonomousCommand);
+    }
   }
 
   /*
    * @Override
    * public void autonomousPeriodic() {
-   *
    * }
    *
    * @Override
